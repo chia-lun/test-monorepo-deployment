2d4c7eea278456c292fe76a6ccb7e3815f1fb78c
James.Y <cyang4@macalester.edu>
2024-06-05 12:05:51 -0500

    feat: added fetch call inside endpoint

--- apps/web/app/api/batch/sortContracts/route.ts
+++ apps/web/app/api/batch/sortContracts/route.ts
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
 
-console.log("access")
+console.log("access");
 // POST handler
 // export async function POST(request: NextRequest) {
 //   const body = {
@@ -28,17 +28,24 @@ console.log("access")
 // }
 
 export async function GET(request: Request) {
-  // const body = {
-  //   contractBatch: [
-  //     "test-contract-1",
-  //     "test-contract-2",
-  //     "test-contract-3",
-  //     "test-contract-4",
-  //     "test-contract-5",
-  //   ],
-  // };
+  const body = {
+    contractBatch: [
+      "test-contract-1",
+      "test-contract-2",
+      "test-contract-3",
+      "test-contract-4",
+      "test-contract-5",
+    ],
+  };
 
-  alert("arrive here");
+  const res = await fetch(`https://webhook.site/141f17ba-7ca7-4733-b3f7-680f92231b9f`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  const data = await res.json();
+
   console.log("arrive here");
-  return Response.json({ message: 'connected to api endpoint' })
+  return Response.json({ message: "connected to api endpoint", data: data});
 }
