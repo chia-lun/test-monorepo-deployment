a606add1f16a4fad0fb3c904e07c3a548e0e98ff
James.Y <cyang4@macalester.edu>
2024-06-06 14:18:01 -0500

    fix: changed to supabase ssr

--- apps/web/app/api/batch/sortContracts/route.ts
+++ apps/web/app/api/batch/sortContracts/route.ts
@@ -1,38 +1,70 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
 import { createClient } from "@supabase/supabase-js";
+import {createServerClient, type CookieOptions} from "@supabase/ssr";
 
 // Initialize the client with your Supabase project URL and API key
-const supabase = createClient(
-  "https://gkdwjjudoifxmgtonxlq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZHdqanVkb2lmeG1ndG9ueGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNDY1NzksImV4cCI6MjAzMjYyMjU3OX0.7K_aJvyU80D1gwPZE6VYlGVYmG3P6wpssC90ahMTtpo",
-  { db: { schema: "public" } }
-);
+// const supabase = createClient(
+//   "https://gkdwjjudoifxmgtonxlq.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZHdqanVkb2lmeG1ndG9ueGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNDY1NzksImV4cCI6MjAzMjYyMjU3OX0.7K_aJvyU80D1gwPZE6VYlGVYmG3P6wpssC90ahMTtpo",
+//   { db: { schema: "public" } }
+// );
 
-async function postData(dataToPost: any) {
-  const { data, error } = await supabase.from("test_batch").insert(dataToPost);
+// async function postData(dataToPost: any) {
+//   const { data, error } = await supabase.from("test_batch").insert(dataToPost);
 
-  if (error) {
-    console.error("Error posting data:", error);
-    return;
-  }
+//   console.log("data:" + data + "," + "error:" + error);
+//   if (error) {
+//     console.error("Error posting data:", error);
+//     return;
+//   }
 
-  console.log("Data posted successfully:", data);
-}
+//   console.log("Data posted successfully:", data);
+// }
 
+//ssr running on external server
 
 export async function GET(request: Request) {
+  const cookieStore = cookies();
+  const supabase = createServerClient(
+    "https://gkdwjjudoifxmgtonxlq.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZHdqanVkb2lmeG1ndG9ueGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNDY1NzksImV4cCI6MjAzMjYyMjU3OX0.7K_aJvyU80D1gwPZE6VYlGVYmG3P6wpssC90ahMTtpo",
+    {
+      db: { schema: "public" },
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    }
+  );
 
   const body = [
-    { contract: ''+ Math.floor(100000000 + Math.random() * 900000000) },
-    { contract: ''+ Math.floor(100000000 + Math.random() * 900000000) },
-    { contract: ''+ Math.floor(100000000 + Math.random() * 900000000) },
-    { contract: ''+ Math.floor(100000000 + Math.random() * 900000000) },
-    { contract: ''+ Math.floor(100000000 + Math.random() * 900000000) },
+    { contract: "" + Math.floor(100000000 + Math.random() * 900000000) },
+    { contract: "" + Math.floor(100000000 + Math.random() * 900000000) },
+    { contract: "" + Math.floor(100000000 + Math.random() * 900000000) },
+    { contract: "" + Math.floor(100000000 + Math.random() * 900000000) },
+    { contract: "" + Math.floor(100000000 + Math.random() * 900000000) },
   ];
-  console.log(body)
-  postData(body);
+
+  try {
+    const { data, error } = await supabase.from("test_batch").insert(body).select();
+    console.log("data:" + data + "," + "error:" + error);
+    if (error) {
+      console.error("Error posting data:", error);
+      throw error;
+    }
+  } catch (error) {
+    //@ts-ignore
+    return NextResponse.json({ status: 500, statusText: error.message });
+  }
 
   return Response.json({ message: "connected to api endpoint" });
 }
