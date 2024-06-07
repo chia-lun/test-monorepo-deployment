a71f518e7a7e0d80cfb42bd53bbdab81a5487b69
James.Y <cyang4@macalester.edu>
2024-06-07 14:58:34 -0500

    feat: cleaned up sortContracts/route.ts

--- apps/web/app/api/batch/sortContracts/route.ts
+++ apps/web/app/api/batch/sortContracts/route.ts
@@ -1,27 +1,7 @@
 import { cookies, headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
-import { createClient } from "@supabase/supabase-js";
-import {createServerClient, type CookieOptions} from "@supabase/ssr";
-
-// Initialize the client with your Supabase project URL and API key
-// const supabase = createClient(
-//   "https://gkdwjjudoifxmgtonxlq.supabase.co",
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZHdqanVkb2lmeG1ndG9ueGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNDY1NzksImV4cCI6MjAzMjYyMjU3OX0.7K_aJvyU80D1gwPZE6VYlGVYmG3P6wpssC90ahMTtpo",
-//   { db: { schema: "public" } }
-// );
-
-// async function postData(dataToPost: any) {
-//   const { data, error } = await supabase.from("test_batch").insert(dataToPost);
-
-//   console.log("data:" + data + "," + "error:" + error);
-//   if (error) {
-//     console.error("Error posting data:", error);
-//     return;
-//   }
-
-//   console.log("Data posted successfully:", data);
-// }
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 
 //ssr running on external server
 
@@ -55,7 +35,10 @@ export async function GET(request: Request) {
   ];
 
   try {
-    const { data, error } = await supabase.from("test_batch").insert(body).select();
+    const { data, error } = await supabase
+      .from("test_batch")
+      .insert(body)
+      .select();
     console.log("data:" + data + "," + "error:" + error);
     if (error) {
       console.error("Error posting data:", error);
